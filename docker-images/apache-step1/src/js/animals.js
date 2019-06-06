b792e12d551dbf2e1c045fd465df73030d635cd1
Tsimwi <caroline.monthoux@heig-vd.ch>
2019-06-06 09:37:37 +0200

    modifies dockerfile

--- docker-images/apache-step1/src/js/animals.js
+++ docker-images/apache-step1/src/js/animals.js
@@ -1,24 +1,26 @@
-$(function() {
+$(function () {
 
-  console.log("Loading animals");
-  function loadAnimals() {
-      $.getJSON("/api/animals/", function( animals){
-        console.log(animals);
-        $("#customContent").empty();
-        var message ="";
-        if( animals.length > 0){
-          for( var i = 0; i < animals.length ; ++i){
-            message += "<p>" + animals[i].firstName + " who is a " + animals[i].typeAnimals + "</p>"; 
-          }
+    console.log("Loading animals");
 
-        }else{
-          message = "No animals are there";
-      }
-      $("#customContent").append(message);
-      
-      
-    });
-  };
-loadAnimals();
-setInterval( loadAnimals, 5000);
+    function loadAnimals() {
+        $.getJSON("/api/animals/", function (animals) {
+            console.log(animals);
+            $("#customContent").empty();
+            var message = "";
+            if (animals.length > 0) {
+                for (var i = 0; i < animals.length; ++i) {
+                    message += "<p>" + animals[i].firstName + " who is a " + animals[i].typeAnimals + "</p>";
+                }
+
+            } else {
+                message = "No animals are there";
+            }
+            $("#customContent").append(message);
+
+
+        });
+    };
+
+    loadAnimals();
+    setInterval(loadAnimals, 5000);
 });
